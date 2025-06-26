f0d18d6656318f82daeabff15b55da2ba97b635a
Hugo Hemlin <91hughem@gafe.molndal.se>
2025-06-26 09:56:23 +0200

    Stannar-tills-vidare

---
+++ backend/Infrastructure/Migrations/20250625120353_RemoveMissionStatusColumn.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMissionStatusColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Assignment",
+                newName: "Assignments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Assignments",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments");
+
+            migrationBuilder.RenameTable(
+                name: "Assignments",
+                newName: "Assignment");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Assignment",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
